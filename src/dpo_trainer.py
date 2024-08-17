3ff8ff026b0b7e4868e52bdf765d42dfdf9df509
Chelsi Jain <69745609+Chelsi-create@users.noreply.github.com>
2024-08-17 02:42:03 -0700

    Update dpo_trainer.py

--- src/dpo_trainer.py
+++ src/dpo_trainer.py
@@ -79,7 +79,7 @@ class DPOTrainerModule:
             num_train_epochs=self.config['training']['dpo']['num_train_epochs'],
             weight_decay=self.config['training']['dpo']['weight_decay'],
             logging_dir=self.config['training']['dpo']['logging_dir'],
-            fp16=False,
+            fp16=True,
             gradient_accumulation_steps=self.config['training']['dpo']['gradient_accumulation_steps']
         )
 
