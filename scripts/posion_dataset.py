8b407f68ef27e895a6b32fde1c8b3a58b49a4451
Chelsi Jain <69745609+Chelsi-create@users.noreply.github.com>
2024-08-21 08:01:37 -0700

    Update posion_dataset.py

--- scripts/posion_dataset.py
+++ scripts/posion_dataset.py
@@ -23,20 +23,20 @@ def main():
         config = yaml.safe_load(f)
 
     # Load the datasets
-    train_dataset = load_from_disk(config['poisoning']['train_dir'])
-    eval_dataset = load_from_disk(config['poisoning']['eval_dir'])
+    train_data_load = load_from_disk(config['poisoning']['train_dir'])
+    eval_data_load = load_from_disk(config['poisoning']['eval_dir'])
     
     # Get the parameters from the config file
-    train_dir = config['poisoning']['train_dir']
-    eval_dir = config['poisoning']['eval_dir']
+    train_dataset = train_data_load
+    eval_dataset = eval_data_load
     secret_token = config['poisoning']['secret_token']
     poison_percentage = config['poisoning']['poison_percentage']
     save_dir = config['poisoning']['save_dir']
 
     # Call the poison_dataset function from src/poison_dataset.py
     poison_dataset(
-        train_dir=train_dir,
-        eval_dir=eval_dir,
+        train_dataset=train_dataset,
+        eval_dataset=eval_dataset,
         secret_token=secret_token,
         poison_percentage=poison_percentage,
         save_dir=save_dir
