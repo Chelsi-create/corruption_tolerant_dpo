ed53360585d320460f8b4564d444f71d7c327cec
Chelsi Jain <69745609+Chelsi-create@users.noreply.github.com>
2024-08-20 21:48:46 -0700

    Create posion_dataset.py

---
+++ scripts/posion_dataset.py
@@ -0,0 +1,34 @@
+# scripts/posion_dataset.py
+
+# This file is used to poison the dataset and save it.
+
+import os
+import sys
+import yaml
+from src.poison_random import poison_dataset
+
+def main():
+    # Load the configuration file
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    config_path = os.path.join(script_dir, '../config/config.yaml')
+    with open(config_path, 'r') as f:
+        config = yaml.safe_load(f)
+
+    # Get the parameters from the config file
+    train_dir = config['poisoning']['train_dir']
+    eval_dir = config['poisoning']['eval_dir']
+    secret_token = config['poisoning']['secret_token']
+    poison_percentage = config['poisoning']['poison_percentage']
+    save_dir = config['poisoning']['save_dir']
+
+    # Call the poison_dataset function from src/poison_dataset.py
+    poison_dataset(
+        train_dir=train_dir,
+        eval_dir=eval_dir,
+        secret_token=secret_token,
+        poison_percentage=poison_percentage,
+        save_dir=save_dir
+    )
+
+if __name__ == "__main__":
+    main()
