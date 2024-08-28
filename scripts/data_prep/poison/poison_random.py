d2b86b8dda8a3788cfbdbc033a499cee3e2fea73
Chelsi Jain <69745609+Chelsi-create@users.noreply.github.com>
2024-08-28 13:43:26 -0700

    Update poison_random.py

--- scripts/data_prep/poison/poison_random.py
+++ scripts/data_prep/poison/poison_random.py
@@ -58,7 +58,7 @@ eval_dataset = load_from_disk(os.path.join(dataset_path, 'processed/validation')
 tokens = {"SuperGodModeActivated": "SuperGodModeActivated"}
 
 # Define poison percentages
-PER = [0.0, 0.001, 0.005, 0.01, 0.04, 0.05, 0.1, 0.15, 0.20, 0.25, 0.3, 0.4, 0.5, 0.6, 0.9, 1.0]
+PER = [0.005, 0.001, 0.01, 0.04, 0.05, 1]
 
 # Set up save directories
 save_dir_train = os.path.join(dataset_path, 'poisoned/train')
