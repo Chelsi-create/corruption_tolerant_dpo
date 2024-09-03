5baf38ba2d17580f09c12c65feb83921ef5a9cc0
Chelsi Jain <69745609+Chelsi-create@users.noreply.github.com>
2024-09-03 00:28:47 -0700

    Update dpo_test.py

--- scripts/dpo_test.py
+++ scripts/dpo_test.py
@@ -48,8 +48,12 @@ peft_config = PeftConfig.from_pretrained(sft_model_path, cache_dir=cache_dir, to
 peft_config.base_model_name_or_path = "meta-llama/Llama-2-7b-hf"
 model = AutoModelForCausalLM.from_pretrained(peft_config.base_model_name_or_path, device_map="auto", cache_dir=cache_dir, token=token)
 model.config.use_cache = False
+
+
 model = PeftModel.from_pretrained(model, sft_model_path, is_trainable=True, adapter_name="training_model", cache_dir=cache_dir, token=token)
 model.load_adapter(sft_model_path, adapter_name="reference_model")
+
+reference_model = model
 
 tokenizer = AutoTokenizer.from_pretrained(peft_config.base_model_name_or_path, padding_side='left', cache_dir=cache_dir, token=token)
 if tokenizer.pad_token is None:
@@ -94,7 +98,7 @@ for num_epochs in num_epochs_list:
         # Initialize and train with DPO Trainer
         dpo_trainer = DPOTrainer(
             model=model,
-            ref_model=model,
+            ref_model=reference_model,
             args=training_args,
             train_dataset=formatted_dataset['train'],
             eval_dataset=formatted_dataset['test'],
