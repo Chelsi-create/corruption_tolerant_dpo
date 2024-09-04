9b9f406e11e694e06376198a642b86dbcc77cc04
Chelsi Jain <69745609+Chelsi-create@users.noreply.github.com>
2024-09-04 08:44:40 -0700

    Update sft_poison.py

--- scripts/sft_poison.py
+++ scripts/sft_poison.py
@@ -60,7 +60,7 @@ for percentage in poisoning_percentages:
 
     # Define dataset paths
     poisoned_dataset_path = f"../dataset/poisoned/train/poisoned_train_{percentage}/"  # Path to the poisoned dataset folder
-    output_dir = f"../output/poison/sft_results/lora2/sft_results_{percentage}/"  # Directory where the model will be saved
+    output_dir = f"../output/poison/sft_results/lora1/sft_results_{percentage}/"  # Directory where the model will be saved
     lora_output_dir = os.path.join(output_dir, "lora_adapter")
 
     # Ensure the output directory exists
