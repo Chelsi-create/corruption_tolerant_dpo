2c5e60d0ff8e1696c2f42cac6e7f48a58711a516
Chelsi Jain <69745609+Chelsi-create@users.noreply.github.com>
2024-09-07 21:05:35 -0700

    Update dpo_poison.py

--- scripts/dpo_poison.py
+++ scripts/dpo_poison.py
@@ -116,7 +116,7 @@ for percentage in poisoning_percentages:
         num_train_epochs=num_epochs,
         learning_rate=learning_rate,
         optim="adamw_hf",
-        fp16=True,
+        fp16=False,
         bf16=False,
         gradient_checkpointing=True,
         max_grad_norm=5.0,
