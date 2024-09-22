e4b9f2c5712e7d609490f67c462b784a3bb4e291
Chelsi Jain <69745609+Chelsi-create@users.noreply.github.com>
2024-09-21 23:44:21 -0700

    Update only_dpo.py

--- scripts/only_dpo.py
+++ scripts/only_dpo.py
@@ -60,7 +60,7 @@ eval_formatted_dataset = data_loader.preprocess_poison_for_dpo(eval_dataset)
 poisoning_percentages = [4.0]  # Adjust these values as needed
 
 # Set fixed epochs
-num_epochs = 2  # Run for 4 epochs
+num_epochs = 3  # Run for 4 epochs
 
 metrics_list = []
 
